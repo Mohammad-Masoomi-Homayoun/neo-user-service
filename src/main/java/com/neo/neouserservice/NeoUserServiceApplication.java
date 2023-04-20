ff9dcc34120e097d0e5be4b67fa3c4f749cc4e12
Homayoun <en.masoomi@gmail.com>
2023-04-21 00:27:05 +0330

    Initial

---
+++ src/main/java/com/neo/neouserservice/NeoUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.neo.neouserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neo/neouserservice/NeoUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.neouserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
